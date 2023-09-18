1f88c2df9560358aa4b8b52bd379021defce1f00
Conor Gleeson <74910941+ConorGleeson@users.noreply.github.com>
2023-09-18 15:51:25 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -43,12 +43,30 @@ function addColleague(colleagues : Colleague[], name: string, department: string
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
+// function sortColleagues(
+//      colleagues: Colleague[],
+//      sorter: (c1: Colleague, c2: Colleague) => number,
+//      max? : number     // CHANGED
+//    ): EmailContact[] {
+//      const end = max < 2 ? 1 : max
+//      const sorted = colleagues.sort(sorter);
+//      const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+//      return fullResult.slice(0,end)
+//    }
+//    // Test invocations
+//    console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+//    console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+
+
 function sortColleagues(
      colleagues: Colleague[],
      sorter: (c1: Colleague, c2: Colleague) => number,
-     max : number
+     max? : number
    ): EmailContact[] {
-     const end = max < 2 ? 1 : max
+     let end = colleagues.length;
+     if (max !== undefined) {
+        end = max < 2 ? 1 : max
+     }
      const sorted = colleagues.sort(sorter);
      const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
      return fullResult.slice(0,end)
@@ -56,9 +74,11 @@ function sortColleagues(
    // Test invocations
    console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
    console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
-
-
+   console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
    
+
+
+
 
 function findFriends(  friends : Friend[], finder :  (friend : Friend) => boolean) : string[]{
      return friends.filter(finder).map(friend => friend.name)
@@ -66,4 +86,15 @@ function findFriends(  friends : Friend[], finder :  (friend : Friend) => boolea
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend : Friend , newInterest : string ) : string[]{
+     if(friend.interests){
+          friend.interests.push(newInterest)
+     }else{
+     friend.interests = [newInterest]
+     }
+ return friend.interests;
+}
+
+console.log(addInterest(friends[1], 'Politics'))
    
